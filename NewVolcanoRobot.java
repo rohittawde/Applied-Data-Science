905dc678a402bd4c93986e45b6c1126c602372f4
rohittawde <32336458+rohittawde@users.noreply.github.com>
2018-05-21 15:15:25 -0400

    Up and running

--- NewVolcanoRobot.java
+++ NewVolcanoRobot.java
@@ -1,42 +1,42 @@
 // Practicing codes from SAMS Teach Yourself JAVA in 21 days
-class NewVolcanoRobot { 
+class NewVolcanoRobot {
 	String status;
 	int speed;
 	float temperature;
 
 	void checkTemperature() {
 		if (temperature > 660) {
-			status = “returning home”;
+			status = "returning home";
 			speed = 5;
 		}
 	}
 
 	void showAttributes() {
-		System.out.println(“Status: ” + status);
-		System.out.println(“Speed: ” + speed);
-		System.out.println(“Temperature: ” + temperature);
+		System.out.println("Status: " + status);
+		System.out.println("Speed: " + speed);
+		System.out.println("Temperature: " + temperature);
 	}
-	
+
 	public static void main (String[] arguments) {
 		NewVolcanoRobot dante = new NewVolcanoRobot();
-		dante.status = “exploring”;
+		dante.status = "exploring";
 		dante.speed = 3;
 		dante.temperature = 550;
 
-		System.out.println(“\n Dante”);
-		dante.showAtributes();
-		
-		System.out.println(“Changing temperature to 670 K”);
+		System.out.println("\n Dante");
+		dante.showAttributes();
+
+		System.out.println("Changing temperature to 670 K");
 		dante.temperature = 670;
-		
-		System.out.println(“Checking temperature”);
+
+		System.out.println("Checking temperature");
 		dante.checkTemperature();
 
-		System.out.println(“\n Dante”);
+		System.out.println("\n Dante");
 		dante.showAttributes();
 
 		NewVolcanoRobot vigil = new NewVolcanoRobot();
-		vigil.status = “coding on a Mac”;
+		vigil.status = "coding on a Mac";
 		vigil.speed = 5;
 		vigil.temperature = 97;
 		vigil.showAttributes();
